b7cac407a22151b25ff3ab4ddb277d10bc23ec04
rafsan3503 <rafsanchowdhuryrudro@gmail.com>
2022-11-24 01:45:49 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middleWare
 app.use(cors());
@@ -13,6 +14,27 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Phonomania is running");
 });
+
+// mongodb connection
+
+const uri =
+  "mongodb+srv://<username>:<password>@cluster0.nk3n7xe.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// run function
+async function run() {
+  try {
+    const userCollection = client.db("phonomania").collection("users");
+  } finally {
+  }
+}
+
+// function call
+run().catch((err) => console.log(err.message));
 
 // listen server
 app.listen(port, () => {
